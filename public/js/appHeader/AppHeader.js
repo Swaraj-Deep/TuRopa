9dc180f610ed7d909fcee26cc1d1506ec93eeddd
Swaraj Deep <swarajdeep33445@gmail.com>
2023-12-16 23:42:51 +0530

    Update basic header

--- public/js/appHeader/AppHeader.js
+++ public/js/appHeader/AppHeader.js
@@ -1,11 +1,24 @@
 const header = document.createElement('header');
 const headerStyleSheet = document.createElement('link');
+const globalStyleSheet = document.createElement('link');
+
+header.classList.add('flex', 'items-baseline', 'justify-between', 'z-max');
 
 headerStyleSheet.setAttribute('rel', 'stylesheet');
 headerStyleSheet.setAttribute('href', './js/appHeader/appHeader.css');
+globalStyleSheet.setAttribute('rel', 'stylesheet');
+globalStyleSheet.setAttribute('href', './css/globals.css');
 
 header.innerHTML = `
   <h1>Tu Ropa</h1>
+  <ul>
+    <li>
+      <a>Cart</a>
+    </li>
+    <li>
+      <a>Login</a>
+    </li>
+  </ul>
 `;
 
 class AppHeader extends HTMLElement {
@@ -17,6 +30,7 @@ class AppHeader extends HTMLElement {
     const rootContainer = this.attachShadow({ mode: 'closed' });
     rootContainer.appendChild(header);
     rootContainer.appendChild(headerStyleSheet);
+    rootContainer.appendChild(globalStyleSheet);
   }
 }
 
